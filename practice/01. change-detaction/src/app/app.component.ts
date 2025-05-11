c10899ff15a3649f87c6364ff235eb480bc3d1f3
AABHAAS MALHOTRA <aabhaas9413@gmail.com>
2025-05-11 22:12:17 +0100

    Updaying pacxkage

--- practice/01. change-detaction/src/app/app.component.ts
+++ practice/01. change-detaction/src/app/app.component.ts
@@ -1,16 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, ChangeDetectorRef, inject } from '@angular/core';
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   // 1. add change detection strategy here
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  counter = 0;
+  counter = 0; 
+  counter$ = interval(1000);
 
   // 2. inject ChangeDetectorRef here
   changeDetector = inject(ChangeDetectorRef);
