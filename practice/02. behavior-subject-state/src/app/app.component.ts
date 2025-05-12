3b1c5e680dd7499db3712d130df2e2660b27932e
AABHAAS MALHOTRA <aabhaas9413@gmail.com>
2025-05-12 23:37:53 +0100

    Combine latest example with  behaviourSubject

--- practice/02. behavior-subject-state/src/app/app.component.ts
+++ practice/02. behavior-subject-state/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { interval } from 'rxjs';
+import { BehaviorSubject, combineLatest, interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,15 +12,21 @@ import { interval } from 'rxjs';
 })
 export class AppComponent {
   readonly counter$ = interval(1000);
+  readonly options$ = new BehaviorSubject<{ [key: string]: string }>({'r': 'red', 'g': 'green', 'b': 'blue'});
+  readonly selected$ = new BehaviorSubject<string>('r');
+
+
+  readonly selectedValue$ = combineLatest([this.options$, this.selected$]).pipe(
+    map(([options, selected]) => options[selected])
+  );
 
   // 2. Remove the counter property and the constructor
   counter = 0;
 
   constructor() {
-    this.counter$.subscribe((value) => {
-      this.counter = value;
-    });
-  }
+ this.selectedValue$.subscribe(console.log);
+    }
+  
 
   // 3. In the html, bind directly to the counter$ observable using the async pipe
 }
