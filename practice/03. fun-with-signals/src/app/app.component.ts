f2def4aa59362c46244c5a1b738fb23e0939abb1
AABHAAS MALHOTRA <aabhaas9413@gmail.com>
2025-05-13 23:37:55 +0100

    Updating signals.

--- practice/03. fun-with-signals/src/app/app.component.ts
+++ practice/03. fun-with-signals/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -11,28 +11,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   // 1. replace with a writeable signal with an initial value of 0
-  readonly firstNumber = undefined; 
+  readonly firstNumber = signal(0); 
 
   // 2. replace with a writeable signal with an initial value of 0
-  readonly secondNumber = undefined;
+  readonly secondNumber = signal(0);
 
   // 3. replace with a computed signal that emits the sum of the first and second numbers
-  readonly sum = undefined;
+  readonly sum = this.firstNumber() + this.secondNumber();
 
   setSecondSignalTo10() {
     // 4. set the second number signal to 10
+    this.secondNumber.set(10);
   }
 
   incrementFirstSignal() {
     // 5. increment the first number signal by 1 but only if it's less than 10
+    if (this.firstNumber() < 10) {
+      this.firstNumber.set(this.firstNumber() + 1);
+    }
   }
 
   incrementBothSignals() {
     // 6. increment both number signals by 1 with a maximum of 10
+    if (this.firstNumber() < 10) {
+      this.firstNumber.set(this.firstNumber() + 1);
+    }
+    if (this.secondNumber() < 10) {
+      this.secondNumber.set(this.secondNumber() + 1);
+    }
   }
 
 
   constructor() {
     // 7. Define an effect that displays both signals to the console whenever any of them changes
+    
   }
 }
