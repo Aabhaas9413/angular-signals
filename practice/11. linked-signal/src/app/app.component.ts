23441e91da3de432bf30a5a75e68890a408e4967
AABHAAS MALHOTRA <aabhaas9413@gmail.com>
2025-05-17 13:33:20 +0100

    Linked signals demo with source and computation included.

--- practice/11. linked-signal/src/app/app.component.ts
+++ practice/11. linked-signal/src/app/app.component.ts
@@ -11,17 +11,33 @@ import { PRODUCTS } from './products';
 export class AppComponent {
   readonly products = signal(['Apple', 'Banana', 'Cherry']);
 
-  readonly selectedProduct = signal('Apple');
+  //readonly selectedProduct = signal('Apple');
 
   /* 1. Create a simple linked signal that sets the selected product to the first
         product in the list., wheven the inventory changes */
+  //readonly selectedProduct = linkedSignal(() => this.products()[0]);      
 
   /* 2. Change the `linkedSignal` so you use the second signature, supply an object 
         with source and computation properties */
+  // readonly selectedProduct = linkedSignal({
+  //   source: () => this.products(),
+  //   computation: (products) => products[0]
+  // });
 
   /* 3. In the computation, use the previous value, to check if the selected product
         is still in the list, if not, set the selected product to the first product in the list */
-
+  readonly selectedProduct = linkedSignal<string[], string>({
+    source: () => this.products(),
+    computation: (products, prev) => {
+      if (products.length === 0) {
+        return '';
+      }
+      if (prev && products.includes(prev.value)) {
+        return prev.value;
+      }
+      return products[0];
+    }
+  })
 
   addProduct() {
     this.products.update(prods => [...prods, PRODUCTS[prods.length]]);
