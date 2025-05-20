5373be3ccea8b6feba138f9d28b6f3e53cf3a8ca
AABHAAS MALHOTRA <aabhaas9413@gmail.com>
2025-05-20 23:26:51 +0100

    using toSignal and toObservable

--- practice/05. rxjs-interop/src/app/app.component.ts
+++ practice/05. rxjs-interop/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, signal } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,16 +12,35 @@ import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 })
 export class AppComponent implements OnInit {
   // 1. Create an observable called number$ that emits an integer value every second
+  number$ = new Observable<number>(subscriber => {
+    let count = 0;
+    const intervalId = setInterval(() => {
+      subscriber.next(count++);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  });
 
   // 2. Convert the observable to a signal called number from the number$ observable.
+  number = toSignal(this.number$);
 
   // 3. Add an element in the UI that displays the value of the 'number' signal.
-
+  
 
   readonly myName = signal('John Doe');
   // 4. Create an observable called myName$ from the "myName" signal
 
+  myName$ = toObservable(this.myName);
+
   // 5. Subscribe to myName$ and log the value to the console so that you log every name change from the UI.  
+  /**
+   *
+   */
+  constructor() {
+     this.myName$.subscribe(name => {
+    console.log('Name changed:', name);
+    });
+  }
 
 
   ngOnInit() {
