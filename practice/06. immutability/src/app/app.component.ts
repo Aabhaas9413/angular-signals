31fbc93018b71d52394dbe4305386f2886f9521d
AABHAAS MALHOTRA <aabhaas9413@gmail.com>
2025-05-21 23:12:58 +0100

    immutabnility with arrays/obects when used with signals.

--- practice/06. immutability/src/app/app.component.ts
+++ practice/06. immutability/src/app/app.component.ts
@@ -15,22 +15,24 @@ export class AppComponent {
   constructor() {
     setTimeout(() => {
       // 1. Add a new name to the array
+      this.names.update((names) => [...names, 'David']);
 
     }, 1000);
 
     setTimeout(() => {
       // 2. Change the name of the person while keeping other properties intact
-
+      this.person.update((person) => ({ ...person, name: 'David' }));
     }, 2000);
 
     setTimeout(() => {
       // 3. Convert all the names in the array to uppercase
-
+      this.names.update((names) => names.map((name) => name.toUpperCase()));
+      this.names.update( names=> names.map(name => name[0].toLocaleLowerCase() + name.slice(1)) );
     }, 3000);
 
     setTimeout(() => {
       // 4. Remove from the array, the name that begins with 'B'
-      
+      this.names.update((names) => names.filter((name) => name[0] !== 'b'));
     }, 4000);
   }
 }
